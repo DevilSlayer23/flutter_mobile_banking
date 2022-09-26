10b1e8b8d492ea8ebdabf128cca16c93883e5b60
P1s44ch <sagarthapa12398@gmail.com>
2022-09-26 15:00:31 +0545

    Initial Commit

---
+++ lib/services/API/api.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class api {
+  late SharedPreferences pref;
+  var baseUrl;
+  var dio = Dio();
+
+  getSharedPrefernces() async {
+    pref = await SharedPreferences.getInstance();
+    return pref;
+  }
+
+  Future<dynamic> login(String phone_number) async {
+    try {
+      //create a map to store the data
+      var data = {
+        "phone_number": phone_number,
+      };
+      //make a post request to the server
+
+      var response = await dio.post(
+        "${baseUrl}/api/login",
+        data: data,
+      );
+      //if the response is successful
+      if (response.statusCode == 200) {
+        //store the token in shared preferences
+        await pref.setString("token", response.data["token"]);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+}
